1016b33ffb47f8b8acb9dd3507ccb7d0c3a9232f
grigorijobucenie <161481259+grigorijobucenie@users.noreply.github.com>
2024-03-03 17:45:47 +0300

    сделал перенос строки

--- homework/grigorii_sedelnikov/5/lesson5.py
+++ homework/grigorii_sedelnikov/5/lesson5.py
@@ -23,4 +23,5 @@ students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
 print(
-    f"Students {students[0]}, {students[1]}, {students[2]} study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}")
+    f"Students {students[0]}, {students[1]}, {students[2]} study these subjects: {subjects[0]}, {subjects[1]}, "
+    f"{subjects[2]}")
