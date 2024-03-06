eee99ea767541f92e95c0dc10c558734e03011ed
grigorijobucenie <161481259+grigorijobucenie@users.noreply.github.com>
2024-03-06 12:56:52 +0300

    выполнил 7 упражнение

---
+++ homework/grigorii_sedelnikov/7/lesson7.1.py
@@ -0,0 +1,7 @@
+programm_number = 70
+
+input_number = int(input("Введите число: "))
+while programm_number != input_number:
+    print("попробуйте снова")
+    input_number = int(input("Введите число: "))
+print("Поздравляю! Вы угадали!")
