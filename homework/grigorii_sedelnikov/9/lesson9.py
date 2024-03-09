e76168bf74d5a482106e4cf5f2db40519d584035
grigorijobucenie <161481259+grigorijobucenie@users.noreply.github.com>
2024-03-09 21:38:31 +0300

    добавил вывод даты в нужном формате

--- homework/grigorii_sedelnikov/9/lesson9.py
+++ homework/grigorii_sedelnikov/9/lesson9.py
@@ -5,6 +5,7 @@ import datetime
 string_date = "Jan 15, 2023 - 12:05:33"
 date_python = datetime.datetime.strptime(string_date, "%b %d, %Y - %H:%M:%S")
 print(date_python.strftime("%B"))
+print(date_python.strftime("%d.%m.%Y, %H:%M"))
 
 # Map, filter
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32,
