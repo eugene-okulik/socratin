2f35257b38223173e003f2fd2bccb0707ba59786
grigorijobucenie <161481259+grigorijobucenie@users.noreply.github.com>
2024-03-10 17:59:57 +0300

    исправил, запутался в своих экперементальных файлах

--- homework/grigorii_sedelnikov/9/lesson9.py
+++ homework/grigorii_sedelnikov/9/lesson9.py
@@ -10,7 +10,7 @@ print(date_python.strftime("%d.%m.%Y, %H:%M"))
 # Map, filter
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32,
                 30, 28, 24, 23]
-warm_days = list(map(lambda x: x > 28, temperatures))
+warm_days = list(filter(lambda x: x > 28, temperatures))
 
 print(f"Максмиальная температура {max(warm_days)}")
 print(f"Минимальная температура {min(warm_days)}")
