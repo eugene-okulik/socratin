3b396cf6becfce41cd911c127d68df0d084c167e
grigorijobucenie <161481259+grigorijobucenie@users.noreply.github.com>
2024-03-18 22:37:13 +0300

    Update lesson15.py

--- homework/grigorii_sedelnikov/15/lesson15.py
+++ homework/grigorii_sedelnikov/15/lesson15.py
@@ -26,8 +26,7 @@ db.commit()
 
 # Создание группы и прикрепление студента к группе
 cursor.execute(
-    "INSERT INTO st4.groups (title, start_date, end_date) "
-    f"VALUES('тестировщики 2024', '2024-01-01', '2024-12-31')")
+    "INSERT INTO st4.groups (title, start_date, end_date) VALUES('тестировщики 2024', '2024-01-01', '2024-12-31')")
 id_group = cursor.lastrowid
 cursor.execute(f"UPDATE st4.students SET group_id = {id_group} WHERE id = {global_id}")
 db.commit()
