ec02e548de1ff1f8a99a4555a4b20a2b4481aaa6
grigorijobucenie <161481259+grigorijobucenie@users.noreply.github.com>
2024-03-18 22:25:30 +0300

    Update lesson15.py

--- homework/grigorii_sedelnikov/15/lesson15.py
+++ homework/grigorii_sedelnikov/15/lesson15.py
@@ -7,10 +7,10 @@ db = mysql.connect(
     port=25060,
     database='st-onl'
 )
+
 cursor = db.cursor(dictionary=True)
 
 # Создание студента и возврат его id
-cursor = db.cursor()
 name = "Тест"
 second_name = "Тестович"
 cursor.execute(f"INSERT INTO st4.students (name, second_name) VALUES('{name}', '{second_name}')")
@@ -25,7 +25,6 @@ cursor.execute(f"INSERT INTO st4.books (title, taken_by_student_id) VALUES('{boo
 db.commit()
 
 # Создание группы и прикрепление студента к группе
-book_name = ["Тестовая книга", "Тестовая книга 2"]
 cursor.execute(
     f"INSERT INTO st4.groups (title, start_date, end_date) "
     f"VALUES('тестировщики 2024', '2024-01-01', '2024-12-31')")
@@ -34,10 +33,10 @@ cursor.execute(f"UPDATE st4.students SET group_id = {id_group} WHERE id = {globa
 db.commit()
 
 # Создание учебных предметов
-subjects = ["НАукография", "Наукология"]
-cursor.execute(f"INSERT INTO st4.subjets (title) VALUES('{subjects[0]}')")
+subjects = ["Наукография", "Наукология"]
+cursor.execute(f"INSERT INTO st4.subjects (title) VALUES('{subjects[0]}')")
 subjects_id_1 = cursor.lastrowid
-cursor.execute(f"INSERT INTO st4.subjets (title) VALUES('{subjects[1]}')")
+cursor.execute(f"INSERT INTO st4.subjects (title) VALUES('{subjects[1]}')")
 subjects_id_2 = cursor.lastrowid
 db.commit()
 
@@ -53,29 +52,3 @@ db.commit()
 cursor.execute(f"INSERT INTO st4.marks (value, lesson_id, student_id) VALUES(200, {lesson_id_1}, {global_id})")
 cursor.execute(f"INSERT INTO st4.marks (value, lesson_id, student_id) VALUES(200, {lesson_id_2}, {global_id})")
 db.commit()
-
-# Показать оценки студента
-cursor.execute(f"select * from st4.marks where student_id = {global_id}")
-results = cursor.fetchall()
-for rom in results:
-    print(rom)
-
-# Показать книги у студента
-
-cursor.execute(f"select * from st4.books where taken_by_student_id = {global_id}")
-results = cursor.fetchall()
-for rom in results:
-    print(rom)
-
-# Все что есть о студенте в базе
-cursor.execute("select * from st4.students "
-               "Join st4.books on st4.students.id = st4.books.taken_by_student_id "
-               "Join st4.groups on st4.students.group_id = st4.groups.id "
-               "Join st4.subjets on st4.books.title = st4.subjets.title "
-               "Join st4.lessons on st4.subjets.id = st4.lessons.subject_id "
-               "Join st4.marks on st4.lessons.id = st4.marks.lesson_id "
-               f"where students.id = {global_id}")
-final = cursor.fetchall()
-for rom in final:
-    print(rom)
-
