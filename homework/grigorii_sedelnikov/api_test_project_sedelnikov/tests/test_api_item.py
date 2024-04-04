bdff700690cedb5d1ed2160bdd4b8cfe3b229498
grigorijobucenie <161481259+grigorijobucenie@users.noreply.github.com>
2024-04-04 11:15:27 +0300

    Update test_api_item.py

--- homework/grigorii_sedelnikov/api_test_project_sedelnikov/tests/test_api_item.py
+++ homework/grigorii_sedelnikov/api_test_project_sedelnikov/tests/test_api_item.py
@@ -1,17 +1,13 @@
-import pytest
 import allure
-import os
-import sys
+import pytest
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from conftest import create_object_endpoint, update_object_endpoint, delete_object_endpoint
-from conftest import item_id
 
 TEST_DATA = [
     {"name": "acer", "year": 2020, "price": 1000, "cpu_model": "i7", "hard_disk_size": 500},
     {"name": "msi", "year": 2020, "price": 2000, "cpu_model": "i5", "hard_disk_size": 800},
     {"name": "asus", "year": 2021, "price": 3000, "cpu_model": "i7", "hard_disk_size": 1600}
 ]
+
 
 @allure.title("Создание товара методом POST")
 @pytest.mark.parametrize("data", TEST_DATA)
@@ -37,6 +33,7 @@ def test_update_object(item_id, update_object_endpoint):
     update_object_endpoint.check_that_status_is_200()
     update_object_endpoint.check_that_name_is_correct(payload["name"])
 
+
 @allure.title('Изменение объекта методом PATCH')
 def test_patch_objects(item_id, update_object_endpoint):
     data = {
@@ -46,10 +43,9 @@ def test_patch_objects(item_id, update_object_endpoint):
     update_object_endpoint.check_that_status_is_200()
     update_object_endpoint.check_that_name_is_correct(data["name"])
 
+
 @allure.title('Удаление объекта')
 @pytest.mark.critical
 def test_delete_objects(item_id, delete_object_endpoint):
     delete_object_endpoint.delete_item(item_id)
     delete_object_endpoint.check_that_status_is_200()
-
-
